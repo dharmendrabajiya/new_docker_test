b269234f38e1413d065c553ded7d1673bd420d1c
dharmendrabajiya <73024965+dharmendrabajiya@users.noreply.github.com>
2022-06-10 14:36:30 +0530

    code

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return {"msg":"My Name one is Dharmendra bajiya"}
+
+@app.route("/login")
+def index():
+    return {"msg":"My Name one is Dharmendra bajiya"}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=9999)
